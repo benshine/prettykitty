e6034f476ebd74e3c3da55d2fca60fef4c6d604f
Benji Shine <ben@openben.net>
2015-01-10 22:02:21 -0800

    Convert KittyHelpers into an IIFE.

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,35 +1,35 @@
 
-var KittyHelpers = KittyHelpers || {};
+var KittyHelpers = (function () {
+  return {
+    get: function (url) {
+      return $.get.call($, url)
+    },
 
-// TODO: replace all of these jquery calls with functions I write myself
+    encodeUrlParams:   function (params) {
+      return Object.keys(params)
+        .map(function (cur) {  return cur + "=" + params[cur]; })
+        .join('&')
+    },
 
-KittyHelpers.get = function (url) {
-  return $.get.call($, url)
-};
+    getBySelector: function (selector) {
+      var element = document.querySelector(selector);
+      if (element) {
+        return element;
+      } else {
+        throw new Error("Could not find element with selector '" + selector + "`");
+      }
+    },
 
-KittyHelpers.encodeUrlParams =  function (params) {
-  return Object.keys(params)
-    .map(function (cur) {  return cur + "=" + params[cur]; })
-    .join('&')
-};
+    setText: function (selector, value) {
+      KittyHelpers.getBySelector(selector).innerHTML = value;
+    },
 
-KittyHelpers.getBySelector= function (selector) {
-  var element = document.querySelector(selector);
-  if (element) {
-    return element;
-  } else {
-    throw new Error("Could not find element with selector '" + selector + "`");
+    setLink: function (selector, value) {
+      return KittyHelpers.getBySelector(selector).setAttribute('href', value);
+    },
+
+    setImage: function (selector, value) {
+      return KittyHelpers.getBySelector(selector).setAttribute('src', value);
+    }
   }
-};
-
-KittyHelpers.setText = function (selector, value) {
-  KittyHelpers.getBySelector(selector).innerHTML = value;
-};
-
-KittyHelpers.setLink = function (selector, value) {
-  return KittyHelpers.getBySelector(selector).setAttribute('href', value);
-};
-
-KittyHelpers.setImage = function (selector, value) {
-  return KittyHelpers.getBySelector(selector).setAttribute('src', value);
-};
+}());
